0a0e5473c441e34fe0bb4380c1f4bc6d03cad978
Ganna <anna.karpycheva@gmail.com>
2025-03-10 21:05:11 +0000

    Update CreateTables.sql
    Fixed null values after testing and transformed tables to accommodate the transition from multiple user roles to two roles.

--- database/CreateTables.sql
+++ database/CreateTables.sql
@@ -1,79 +1,79 @@
-SHOW DATABASES;
 USE db_seat_select;
 
 DROP TABLE IF EXISTS db_users;
-CREATE TABLE db_users (
-    id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
-    user_login VARCHAR(20) UNIQUE,
-    user_password VARCHAR(8),
-    user_name VARCHAR(20),
-    user_email VARCHAR(50),
-    PRIMARY KEY (id)
-);
-
 DROP TABLE IF EXISTS roles;
-CREATE TABLE roles (
-    id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
-    role_name VARCHAR(50),
-    PRIMARY KEY (id)
-);
-
 DROP TABLE IF EXISTS user_roles;
-CREATE TABLE user_roles (
-    user_id INTEGER NOT NULL,
-    role_id INTEGER NOT NULL,
-    PRIMARY KEY (user_id, role_id),
-    FOREIGN KEY (user_id) REFERENCES db_users(id)
-    ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (role_id) REFERENCES roles(id)
-    ON UPDATE CASCADE ON DELETE CASCADE
-);
-
-
 DROP TABLE IF EXISTS restaurant_tables;
-CREATE TABLE restaurant_tables (
-    id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
-    capacity INTEGER,
-    PRIMARY KEY (id)
-);
-
 DROP TABLE IF EXISTS reservations;
-CREATE TABLE reservations (
-    id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
-    table_id INTEGER,
-    user_id INTEGER,
-    reservation_date DATE,
-    reservation_time TIME,
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES db_users(id)
-    ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (table_id) REFERENCES restaurant_tables(id)
-    ON UPDATE CASCADE ON DELETE CASCADE
-);
-
 DROP TABLE IF EXISTS reviews;
-CREATE TABLE reviews (
-    id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
-    user_id INTEGER,
-    table_id INTEGER,
-    rating INTEGER,
-    review_text TEXT(65535),
-    review_date DATE,
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES db_users(id)
-    ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (table_id) REFERENCES restaurant_tables(id)
-    ON UPDATE CASCADE ON DELETE CASCADE
+DROP TABLE IF EXISTS user_security_questions;
+
+
+CREATE TABLE db_users (
+	id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+	user_login VARCHAR(20) NOT NULL UNIQUE,
+	user_password VARCHAR(8) NOT NULL,
+	user_email VARCHAR(50) NOT NULL,
+	is_admin BOOLEAN NOT NULL, 
+	PRIMARY KEY(id)
 );
 
-DROP TABLE IF EXISTS user_security_questions;
+
+CREATE TABLE restaurant_tables (
+	id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+	capacity INTEGER NOT NULL,
+	PRIMARY KEY(id)
+);
+
+
+CREATE TABLE reservations (
+	id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+	table_id INTEGER NOT NULL,
+	user_id INTEGER NOT NULL,
+	reservation_date DATE NOT NULL,
+	reservation_time TIME NOT NULL,
+	PRIMARY KEY(id)
+);
+
+
+CREATE TABLE reviews (
+	id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+	user_id INTEGER NOT NULL,
+	table_id INTEGER NOT NULL,
+	rating INTEGER NOT NULL,
+	review_text TEXT NOT NULL,
+	review_date DATE NOT NULL,
+	PRIMARY KEY(id)
+);
+
+
 CREATE TABLE user_security_questions (
 	id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
-	user_id INTEGER,
-	question VARCHAR(255),
-	answer VARCHAR(50),
-	PRIMARY KEY(id),
-	FOREIGN KEY (user_id) REFERENCES db_users(id)
-	ON UPDATE CASCADE ON DELETE CASCADE
+	user_id INTEGER NOT NULL,
+	question VARCHAR(255) NOT NULL,
+	answer VARCHAR(50) NOT NULL,
+	PRIMARY KEY(id)
 );
 
+
+ALTER TABLE reservations
+ADD CONSTRAINT fk_reservations_user_id FOREIGN KEY(user_id) REFERENCES db_users(id)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+
+ALTER TABLE reservations
+ADD CONSTRAINT fk_reservations_table_id FOREIGN KEY(table_id) REFERENCES restaurant_tables(id)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+
+
+ALTER TABLE reviews
+ADD CONSTRAINT fk_reviews_user_id FOREIGN KEY(user_id) REFERENCES db_users(id)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+
+ALTER TABLE reviews
+ADD CONSTRAINT fk_reviews_table_id FOREIGN KEY(table_id) REFERENCES restaurant_tables(id)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+
+
+ALTER TABLE user_security_questions
+ADD CONSTRAINT fk_user_security_questions_user_id FOREIGN KEY(user_id) REFERENCES db_users(id)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
