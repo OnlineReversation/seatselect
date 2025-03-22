ec16ba84a92167aa0bcfba77345fcd9f1f84d5ce
GannaUK <anna.karpycheva@gmail.com>
2025-03-22 13:17:22 +0000

    implemented view available tables by date and time

--- api/check_availability.php
+++ api/check_availability.php
@@ -3,15 +3,20 @@
 require_once 'db.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // $date = $_POST['date'] ?? null;
+
     $date = date('Y-m-d', strtotime($_POST['date']));
-    // $time = $_POST['time'] ?? null;
     $time = sprintf("%02d:00:00", $_POST['time']); // Формирует время из строки 10-24
 
     if ($date && $time) {
-        $stmt = $pdo->prepare("SELECT table_id FROM reservations WHERE reservation_date = :date AND reservation_time = :time");
+        // $stmt = $pdo->prepare("SELECT table_id FROM reservations WHERE reservation_date = :date AND reservation_time = :time");
+        $stmt = $pdo->prepare("
+            SELECT r.table_id, t.capacity 
+            FROM reservations r
+            JOIN restaurant_tables t ON r.table_id = t.id
+            WHERE r.reservation_date = :date AND r.reservation_time = :time
+        ");
         $stmt->execute(['date' => $date, 'time' => $time]);
-        $reservedTables = $stmt->fetchAll(PDO::FETCH_COLUMN);
+        $reservedTables = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
         echo json_encode([
             'success' => true,
