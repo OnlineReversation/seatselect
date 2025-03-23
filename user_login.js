a312e671cf43dfa3d4f11fffaf589107226d4966
GannaUK <anna.karpycheva@gmail.com>
2025-03-23 23:10:05 +0000

    added a login check function in user_login.js

--- user_login.js
+++ user_login.js
@@ -2,8 +2,11 @@ window.onload = function () {
   const user_login = localStorage.getItem("user_login");
 
   if (user_login) {
-    document.getElementById("logged_user").textContent =
-      "Logged in: " + user_login;
-    document.getElementById("logged_user").href = "user_panel.html"; // замените на актуальный путь
+    if (user_login) {
+      let loggedUserElements = document.querySelectorAll(".logged_user");
+      loggedUserElements.forEach(function (element) {
+        element.textContent = "Logged in: " + user_login;
+      });
+    }
   }
 };
